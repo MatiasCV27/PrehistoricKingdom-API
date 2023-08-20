27ddf4c1cb11ea16b4442deb70be4e8099fb3823
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-19 23:52:25 -0500

    Especificando Metodos

--- PrehistoricKingdom-API/Controllers/EspecieController.cs
+++ PrehistoricKingdom-API/Controllers/EspecieController.cs
@@ -13,35 +13,35 @@ namespace PrehistoricKingdom_API.Controllers
     public class EspecieController : ApiController
     {
         // GET api/<controller>
-        [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+        [EnableCors(origins: "http://localhost:3000", headers: "Content-Type", methods: "GET")]
         public List<Especie> Get()
         {
             return EspecieData.Listar();
         }
 
         // GET api/<controller>/5
-        [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+        [EnableCors(origins: "http://localhost:3000", headers: "Content-Type", methods: "GET")]
         public Especie Get(int id)
         {
             return EspecieData.Obtener(id);
         }
 
         // POST api/<controller>
-        [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+        [EnableCors(origins: "http://localhost:3000", headers: "Content-Type", methods: "POST")]
         public bool Post([FromBody] Especie oEspecie)
         {
             return EspecieData.Registrar(oEspecie);
         }
 
         // PUT api/<controller>/5
-        [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+        [EnableCors(origins: "http://localhost:3000", headers: "Content-Type", methods: "PUT")]
         public bool Put([FromBody] Especie oEspecie)
         {
             return EspecieData.Editar(oEspecie);
         }
 
         // DELETE api/<controller>/5
-        [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+        [EnableCors(origins: "http://localhost:3000", headers: "Content-Type", methods: "DELETE")]
         public bool Delete(int id)
         {
             return EspecieData.Eliminar(id);
